3f4a47f3980f8d91ea087ecdad0c9d26533008bc
francescocifaldi <francesco.cifaldi@gmail.com>
2024-10-28 15:33:57 +0100

    ADD try to print everything by JS

---
+++ script.js
@@ -0,0 +1,51 @@
+const teamMembers = [
+  {
+    name: "Marco Bianchi",
+    role: "Designer",
+    email: "marcobianchi@team.com",
+    img: "img/male1.png"
+  },
+  {
+    name: "Laura Rossi",
+    role: "Front-end Developer",
+    email: "laurarossi@team.com",
+    img: "img/female1.png"
+  },
+  {
+    name: "Giorgio Verdi",
+    role: "Back-end Developer",
+    email: "giorgioverdi@team.com",
+    img: "img/male2.png"
+  },
+  {
+    name: "Marta Ipsum",
+    role: "SEO Specialist",
+    email: "martarossi@team.com",
+    img: "img/female2.png"
+  },
+  {
+    name: "Roberto Lorem",
+    role: "SEO Specialist",
+    email: "robertolorem@team.com",
+    img: "img/male3.png"
+  },
+  {
+    name: "Daniela Amet",
+    role: "Analyst",
+    email: "danielaamet@team.com",
+    img: "img/female3.png"
+  }
+];
+
+let cardsContainer = document.getElementById('cards');
+for (let i = 0; i < teamMembers.length; i++) {
+  let card = document.createElement("div");
+  card.classList.add("col-lg-4");
+  let cardRows = document.createElement("div");
+  cardRows.classList.add("row");
+  let imgContainer = document.createElement("div");
+  imgContainer.innerHTML = `<img src="./${teamMembers[i].img}">`
+  cardsContainer.append(card, cardRows, imgContainer)
+
+}
+
